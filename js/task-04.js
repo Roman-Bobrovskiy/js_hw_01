dba1e1c976e06f2475b0bf434cb93c00febf9398
RB <bobrovskiyroman@gmail.com>
2021-05-18 09:49:26 +0300

    task4

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+let credits = 35500;
+let pricePerDroid = 3000;
+let totalPrice;
+let message;
+let DroidQuantity = prompt("Enter number");
+totalPrice = DroidQuantity * pricePerDroid;
+console.log(typeof totalPrice);
+if (DroidQuantity === null) {
+  message = "Canceled by user!";
+  alert(message);
+} else if (totalPrice >= credits) {
+  message = "Insufficient Funds!";
+  alert(message);
+} else if (totalPrice <= credits) {
+  alert(
+    "You bought" +
+      " " +
+      DroidQuantity +
+      " " +
+      "droids" +
+      " " +
+      (credits - totalPrice) +
+      " " +
+      "credits left on your account"
+  );
+}
