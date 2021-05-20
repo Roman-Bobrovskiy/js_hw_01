96747272d3ac7f9bcf46ebe1cc59b00c724c3af1
RB <bobrovskiyroman@gmail.com>
2021-05-20 07:35:03 +0300

    new task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,23 +1,80 @@
+let chinaPrice = 150;
+let chilePrice = 250;
+let australiaPrice = 165;
+let indiaPrice = 90;
+let jamaicaPrice = 130;
 let delivery = prompt("Choose a country");
+let deliveryText =
+  delivery.charAt(0).toUpperCase() + delivery.slice(1).toLowerCase();
 switch (delivery.toLowerCase()) {
   case "china":
-    alert("Delivery to the China will cost 150 credits");
+    alert(
+      "Delivery to the" +
+        " " +
+        deliveryText +
+        " " +
+        "will cost " +
+        " " +
+        chinaPrice +
+        " " +
+        " credits"
+    );
     break;
 
   case "chile":
-    alert("Delivery to the Chile will cost 250 credits");
+    alert(
+      "Delivery to the" +
+        " " +
+        deliveryText +
+        " " +
+        "will cost " +
+        " " +
+        chilePrice +
+        " " +
+        " credits"
+    );
     break;
 
   case "australia":
-    alert("Delivery to the Australia will cost 165 credits");
+    alert(
+      "Delivery to the" +
+        " " +
+        deliveryText +
+        " " +
+        "will cost " +
+        " " +
+        australiaPrice +
+        " " +
+        " credits"
+    );
     break;
 
   case "india":
-    alert("Delivery to the India will cost 90 credits");
+    alert(
+      "Delivery to the" +
+        " " +
+        deliveryText +
+        " " +
+        "will cost " +
+        " " +
+        indiaPrice +
+        " " +
+        " credits"
+    );
     break;
 
   case "jamaica":
-    alert("Delivery to the Jamaica will cost 130 credits");
+    alert(
+      "Delivery to the" +
+        " " +
+        deliveryText +
+        " " +
+        "will cost " +
+        " " +
+        jamaicaPrice +
+        " " +
+        " credits"
+    );
     break;
 
   default:
